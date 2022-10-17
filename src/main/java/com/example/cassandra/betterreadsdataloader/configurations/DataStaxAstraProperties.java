7a5f2b87210cab9102b49c5e5ea9f1cd050f2f4a
AbhinashJha342 <abhinashjha09@gmail.com>
2022-10-17 11:45:30 -0500

    changes to start app and connecting to datastax cassandra instance

--- src/main/java/com/example/cassandra/betterreadsdataloader/configurations/DataStaxAstraProperties.java
+++ src/main/java/com/example/cassandra/betterreadsdataloader/configurations/DataStaxAstraProperties.java
@@ -1,2 +1,20 @@
-package com.example.cassandra.betterreadsdataloader.configurations;public class DataStaxAstraProperties {
+package com.example.cassandra.betterreadsdataloader.configurations;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.io.File;
+
+@ConfigurationProperties(prefix="datastax.astra")
+public class DataStaxAstraProperties {
+
+    private File secureConnectBundle;
+
+
+    public File getSecureConnectBundle() {
+        return secureConnectBundle;
+    }
+
+    public void setSecureConnectBundle(File secureConnectBundle) {
+        this.secureConnectBundle = secureConnectBundle;
+    }
 }
